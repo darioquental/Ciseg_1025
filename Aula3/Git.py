4cd48711232d18c0103501ea52929824be5afb20
Dario Quental <dhdq@hotmail.com>
2025-11-05 20:43:12 +0000

    Aula Git

---
+++ Aula3/Git.py
@@ -0,0 +1,9 @@
+# Aula git HUB
+# primeiros passos clone / init  ----> só inicial
+
+
+
+
+# fazer alterações 
+# Add                ## exemplo     git add .
+# commit  add local  ## exemplo     git commit -m "Aula Git"
